fa44945136da7dd7a1ed80f33155627f8df54303
aminofabian <aminofab@gmail.com>
2025-12-04 17:16:51 +0300

    added

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker } from "react-day-picker"
+import { DayPicker, type CustomComponents } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -11,6 +11,7 @@ function Calendar({
   className,
   classNames,
   showOutsideDays = true,
+  components,
   ...props
 }: React.ComponentProps<typeof DayPicker>) {
   return (
@@ -59,14 +60,17 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
-      }}
+      components={
+        {
+          IconLeft: ({ className, ...iconProps }) => (
+            <ChevronLeft className={cn("size-4", className)} {...iconProps} />
+          ),
+          IconRight: ({ className, ...iconProps }) => (
+            <ChevronRight className={cn("size-4", className)} {...iconProps} />
+          ),
+          ...components,
+        } satisfies Partial<CustomComponents>
+      }
       {...props}
     />
   )
